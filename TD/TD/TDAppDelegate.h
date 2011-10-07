dc065bcbc533d8c98d9fc666553f6f67dc541c2c
yvzgrcn <yvzgrcn@gmail.com>
2011-10-07 17:01:51 +0300

    Proje Kuruldu

---
+++ TD/TD/TDAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  TDAppDelegate.h
+//  TD
+//
+//  Created by Gurcan Yavuz on 10/7/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TDAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
---
+++ TD/TDTests/TDTests.m
@@ -0,0 +1,33 @@
+//
+//  TDTests.m
+//  TDTests
+//
+//  Created by Gurcan Yavuz on 10/7/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "TDTests.h"
+
+
+@implementation TDTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in TDTests");
+}
+
+@end
